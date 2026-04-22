e2195109966ca42b533d55feb7a086702208d80e
Matheus Majer <majermatheus@gmail.com>
2026-04-22 17:18:33 +0100

    Day 001: Assigning a variable

---
+++ phase-01-basics/day-001.py
@@ -0,0 +1,39 @@
+"""
+Day 1 – Assigning a Variable
+
+1) Task:
+Create one variable and assign it one value.
+
+2) What I Learnt:
+I learnt that the equal sign "=" is called the assignment operator and is used to assign values to variables in Python. I also learnt that it is best practice to use underscores when a variable name is composed of two or more words (e.g. "my_variable" instead of "myvariable"), and that the name of the variable should be meaningful.
+
+3) Key Concepts:
+- Python operators
+- Variable assignment
+- Data types
+- Variable naming best practices
+
+4) Challenges:
+The challenge I faced was understanding that the equal sign "=" is not used in the conventional way, but instead to express that a variable x holds the value y by assigning y to x. I believe it is intuitive to read a variable as “x is equal to y” (e.g. x = 2 meaning x is equal to 2), especially during early exposure to Python.
+
+5) How I Overcame It:
+To overcome this challenge, I read content from webpages such as:
+- https://www.w3schools.com/python/gloss_python_assignment_operators.asp
+- https://python-reference.readthedocs.io/en/latest/docs/operators/assignment.html
+- https://realpython.com/python-assignment-operator/
+
+I also consulted textbooks such as:
+- Data Structures & Algorithms in Python
+- Think Python: How to Think Like a Computer Scientist
+- Fluent Python
+
+Based on the information I found, I created a variable called my_name to store the value "Matheus".
+
+6) Mistakes:
+No significant mistakes were made at this stage, considering the simplicity of this operator. However, since the variable I created stores a string data type, an error would occur if quotation marks were not added.
+
+7) Improvements:
+No improvements at this stage.
+"""
+
+my_name = "Matheus"

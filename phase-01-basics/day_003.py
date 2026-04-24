fc4b7d44c2d11520f5defdb9b03981353e0a1f42
Matheus Majer <majermatheus@gmail.com>
2026-04-24 16:25:43 +0100

    Day 003: Assigning a string value

---
+++ phase-01-basics/day_003.py
@@ -0,0 +1,30 @@
+"""
+Day 3 – Assigning a String (Reinforcement)
+
+1) Task:
+Create one variable and assign it one string value.
+
+2) What I Learnt:
+As this challenge reinforces assigning a variable with a string data type, it is similar to the Day 001 exercise, where I voluntarily assigned a string without being required to do so. Therefore, I reinforced my learning with a small variation: instead of using double quotes, I used single quotes, which also work in Python.
+
+3) Key Concepts:
+- Python operators
+- Variable assignment
+- Data types
+- Variable naming best practices
+- Use of single quotation marks for strings
+
+4) Challenges:
+This task was straightforward and did not present any major challenges.
+
+5) How I Overcame It:
+No specific action was required, as no challenges were encountered.
+
+6) Mistakes:
+No mistakes were identified during this task.
+
+7) Improvements:
+No immediate improvements were identified for this task.
+"""
+
+monster = 'Godzilla'
